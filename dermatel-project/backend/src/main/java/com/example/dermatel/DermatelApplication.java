fc42cf913943d456ce04457946bd5af3ddb2a65e
jay-abre <jamesjirehabrenica@yahoo.com>
2024-08-17 17:22:00 +0800

    added initial files

---
+++ dermatel-project/backend/src/main/java/com/example/dermatel/DermatelApplication.java
@@ -0,0 +1,13 @@
+package com.example.dermatel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DermatelApplication.class, args);
+	}
+
+}
---
+++ dermatel-project/backend/src/test/java/com/example/dermatel/DermatelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dermatel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
